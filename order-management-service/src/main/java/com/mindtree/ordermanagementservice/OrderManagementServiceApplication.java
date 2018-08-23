33866f5d44bc27fefaafc9210fffc2de42eaa3f9
pavithra <pavithra.hanumegowda@mindtree.com>
2018-08-23 19:31:22 +0530

    Swagger commits

--- order-management-service/src/main/java/com/mindtree/ordermanagementservice/OrderManagementServiceApplication.java
+++ order-management-service/src/main/java/com/mindtree/ordermanagementservice/OrderManagementServiceApplication.java
@@ -2,11 +2,27 @@ package com.mindtree.ordermanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2 
 public class OrderManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementServiceApplication.class, args);
 	}
+	
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.mindtree.ordermanagementservice.api"))
+				.paths(PathSelectors.any()).build();
+	} 
+	
 }
