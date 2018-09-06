84a0e5806f5d3fdf1b603a38883f5d3741638b7e
vicky88377 <vicky88377@users.noreply.github.com>
2018-09-06 10:48:14 +0530

    Update OrderManagementServiceApplication.java

--- order-management-service/src/main/java/com/mindtree/ordermanagementservice/OrderManagementServiceApplication.java
+++ order-management-service/src/main/java/com/mindtree/ordermanagementservice/OrderManagementServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
