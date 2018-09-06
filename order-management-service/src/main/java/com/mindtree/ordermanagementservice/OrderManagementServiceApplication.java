0f3925586c733803174bcfab94b23daff967900c
vicky88377 <vicky88377@users.noreply.github.com>
2018-09-06 14:59:26 +0530

    Update OrderManagementServiceApplication.java

--- order-management-service/src/main/java/com/mindtree/ordermanagementservice/OrderManagementServiceApplication.java
+++ order-management-service/src/main/java/com/mindtree/ordermanagementservice/OrderManagementServiceApplication.java
@@ -33,7 +33,7 @@ public class OrderManagementServiceApplication {
 	private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
             .title("Order Management Service")
-          //  .description("Awesome description")
+           .description("Created by Team (Uday, Shekhar, Viakas, Pavithra)")
             .license("Apache 2.0")
             .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
             .termsOfServiceUrl("")
